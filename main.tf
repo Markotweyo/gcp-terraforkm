23843501f03e565d48a8378d4bf475ee8607eb52
Mark <markotweyo@gmail.com>
2022-07-15 21:09:56 +0300

    Fixed instance group

--- main.tf
+++ main.tf
@@ -20,7 +20,8 @@ resource "google_compute_subnetwork" "subnet" {
  ip_cidr_range = "${var.subnet_cidr}"
  network       = "${var.name}-vpc"
  depends_on    = ["google_compute_network.vpc"]
- region      = "${var.region}"
+ region        = "${var.region}"
+ 
 }
 // VPC firewall configuration
 resource "google_compute_firewall" "firewall" {
@@ -41,10 +42,18 @@ resource "google_compute_firewall" "firewall" {
 }
 
 //Instance
+
+locals {
+	master_instance_virtual_machines = {
+   "master" = { zone = "us-east4-a" },
+   "master-backup" = {zone = "us-east4-b" }
+ }
+}
 resource "google_compute_instance" "vm_instance" {
-  name         = "master"
+  for_each=local.master_instance_virtual_machines
+  name         = each.key
   machine_type = "n2-standard-4"
-  zone			="us-east4-a"
+  zone			=each.value.zone
   tags         = ["web","master-tag"]
 
   boot_disk {
@@ -64,17 +73,17 @@ resource "google_compute_instance" "vm_instance" {
 
 
 
-//Creating Autoscaller resource
+//Creating Autoscaller resource main
+
 resource "google_compute_autoscaler" "default" {
   provider = google-beta
-
-  name   = "my-autoscaler"
-  zone   = "us-east4-a"
-  target = google_compute_instance_group_manager.default.id
+  name     = "my-auto-scaler"
+  zone     = "us-east4-a"
+  target   = google_compute_instance_group_manager.default.id
 
   autoscaling_policy {
     max_replicas    = 5
-    min_replicas    = 1
+    min_replicas    = 3
     cooldown_period = 60
 
     metric {
@@ -82,8 +91,37 @@ resource "google_compute_autoscaler" "default" {
       filter                     = "resource.type = pubsub_subscription AND resource.label.subscription_id = our-subscription"
       single_instance_assignment = 65535
     }
+	cpu_utilization {
+      target = 0.7
+    }
   }
 }
+
+
+//Creating Autoscaller resource main
+
+resource "google_compute_autoscaler" "backup" {
+  provider = google-beta
+  name     = "my-auto-scaler-backup"
+  zone     = "us-east4-b"
+  target   = google_compute_instance_group_manager.backup.id
+
+  autoscaling_policy {
+    max_replicas    = 5
+    min_replicas    = 3
+    cooldown_period = 60
+
+    metric {
+      name                       = "pubsub.googleapis.com/subscription/num_undelivered_messages"
+      filter                     = "resource.type = pubsub_subscription AND resource.label.subscription_id = our-subscription"
+      single_instance_assignment = 65535
+    }
+	cpu_utilization {
+      target = 0.7
+    }
+  }
+}
+
 //Creating Instance Template
 resource "google_compute_instance_template" "default" {
   provider = google-beta
@@ -98,9 +136,8 @@ resource "google_compute_instance_template" "default" {
   }
 
   network_interface {
-    network = "default"
-	##google_compute_network.vpc.name
-	##subnetwork = google_compute_subnetwork.subnet.self_link
+    network = google_compute_network.vpc.name
+	subnetwork = google_compute_subnetwork.subnet.self_link
   }
 
   metadata = {
@@ -115,9 +152,11 @@ resource "google_compute_target_pool" "default" {
   name = "my-target-pool"
 }
 
+
+//Creating compte Instance google_compute_instance_group_manager
 resource "google_compute_instance_group_manager" "default" {
   provider = google-beta
-  name = "my-igm"
+  name = "main-my-igm"
   zone = "us-east4-a"
 
   version {
@@ -136,7 +175,42 @@ resource "google_compute_instance_group_manager" "default" {
     name = "https"
     port = "8443"
   }
+   named_port {
+    name = "ssh"
+    port = "22"
+  }
 }
+
+
+//Creating compte Instance google_compute_instance_group_manager
+resource "google_compute_instance_group_manager" "backup" {
+  provider = google-beta
+  name = "backup-my-igm"
+  zone = "us-east4-b"
+
+  version {
+    instance_template = google_compute_instance_template.default.id
+    name              = "primary"
+  }
+
+  target_pools       = [google_compute_target_pool.default.id]
+  base_instance_name = "autoscaler-sample"
+
+  named_port {
+    name = "http"
+    port = "8080"
+  }
+  named_port {
+    name = "https"
+    port = "8443"
+  }
+   named_port {
+    name = "ssh"
+    port = "22"
+  }
+}
+
+
 
 data "google_compute_image" "debian_9" {
   provider = google-beta
--- provider.tf
+++ provider.tf
@@ -2,12 +2,11 @@ provider "google" {
   credentials = file("C:/Users/Mark/Desktop/terraform-355617-bb4e0d0bb13a.json")
   project = "terraform-355617"
   region  = "us-east4"
-  zone   = "us-east4-a"
+ 
 }
 
 provider "google-beta" {
-	credentials = file("C:/Users/Mark/Desktop/terraform-355617-bb4e0d0bb13a.json")
+  credentials = file("C:/Users/Mark/Desktop/terraform-355617-bb4e0d0bb13a.json")
   region = "us-east4"
-  zone   = "us-east4-a"
   project = "terraform-355617"
 }
